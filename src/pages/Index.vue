56a1d7cf4b9067f87323fffee5c6dff331618c1d
Suxrob9966 <rabbimovich9966@gmail.com>
2022-12-22 09:52:08 -0600

    added header texts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,18 +7,30 @@
         <br />
         ДЛЯ ПРАВИЛЬНЫХ ПРОДУКТОВ
       </div>
-      <div class="title text-center text-h4 text-bold title">
+      <div class="title text-center text-h4 text-bold title q-pb-lg q-ma-lg">
         <span>
           Мы производим пищевую упаковку для молочных предприятий.
         </span>
       </div>
+      <div class="title text-center text-h3 text-bold title-top text-white ">
+        <span class="bg-positive q-pa-md">НАШЕ ПРОИЗВОДСТВО</span>
+      </div>
     </div>
 
-    <div class="row">
-      <div class="col">1</div>
-      <div class="col">1</div>
-      <div class="col">1</div>
+
+    <!-- description -->
+    <div class="row full-width">
+      <div class="col-sm-8">
+        <q-img src="tools/1.png" class="img-center q-ma-lg gt-xs"
+          style="width: 100%;" />
+      </div>
+      <div class="col-sm-4">
+        <div class="row q-px-lg q-pt-lg text-h6 text-left">
+          оснащено лучшим в мире <br /> оборудованием европейского производства
+        </div>
+      </div>
     </div>
+
   </q-page>
 </template>
 
