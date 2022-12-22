3e35744b2537a60f8a54c5ef5735f55ae6c4141e
Suxrob9966 <rabbimovich9966@gmail.com>
2022-12-22 03:47:58 -0600

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Midas Plastics
+        </q-toolbar-title>
+        <div class="text-h6">+xxx xx xxx xxxx</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Contacts</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    return {}
+  },
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center" style="background: #f5f7f5;">
+    <!-- title -->
+    <div class="full-width header">
+      <div class="title text-center text-h3 text-bold title-top text-red q-pb-xl">
+        ПРАВИЛЬНАЯ УПАКОВКА
+        <br />
+        ДЛЯ ПРАВИЛЬНЫХ ПРОДУКТОВ
+      </div>
+      <div class="title text-center text-h4 text-bold title">
+        <span>
+          Мы производим пищевую упаковку для молочных предприятий.
+        </span>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">1</div>
+      <div class="col">1</div>
+      <div class="col">1</div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
+
