fc41aced808e707fa2086cef6caf9b0888fd3a26
Suxrob9966 <rabbimovich9966@gmail.com>
2022-12-23 01:55:49 -0600

    added cardviews

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page class="flex flex-center" style="background: #f5f7f5;">
     <!-- title -->
     <div class="full-width header">
-      <div class="title text-center text-h3 text-bold title-top text-red q-pb-xl">
+      <div class="title text-center text-h3 text-bold title-top text-red q-pa-xl">
         ПРАВИЛЬНАЯ УПАКОВКА
         <br />
         ДЛЯ ПРАВИЛЬНЫХ ПРОДУКТОВ
@@ -19,15 +19,46 @@
 
 
     <!-- description -->
-    <div class="row full-width">
-      <div class="col-sm-8">
-        <q-img src="tools/1.png" class="img-center q-ma-lg gt-xs"
-          style="width: 100%;" />
+    <div class="row full-width q-pt-lg flex-center">
+      <div class="col-sm-7">
+        <q-img src="tools/1.png" class="img-center q-ma-lg gt-xs" style="width: 90%;" />
       </div>
-      <div class="col-sm-4">
-        <div class="row q-px-lg q-pt-lg text-h6 text-left">
-          оснащено лучшим в мире <br /> оборудованием европейского производства
+      <div class="col-sm-5">
+        <div class="row q-px-lg q-pt-lg text-h5 text-left">
+          <div class="q-pa-md">
+            <q-card style="max-width: 500px">
+              <q-card-section>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" rect fill="red" class="bi bi-circle-fill"
+                  viewBox="0 0 16 16">
+                  <circle cx="8" cy="8" r="8" />
+                </svg> оснащено лучшим в мире <br /> оборудованием европейского производства
+              </q-card-section>
+            </q-card>
+          </div>
         </div>
+        <q-separator />
+      </div>
+    </div>
+
+    <div class="row full-width q-pt-md flex-center">
+      <div class="col-sm-5">
+        <div class="row q-px-lg q-pt-lg text-h5 text-left">
+          <div class="q-pa-md">
+            <q-card style="max-width: 500px">
+              <q-card-section>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" rect fill="red" class="bi bi-circle-fill"
+                  viewBox="0 0 16 16">
+                  <circle cx="8" cy="8" r="8" />
+                </svg> обеспечивает высокую производительность
+              </q-card-section>
+            </q-card>
+
+          </div>
+        </div>
+        <q-separator />
+      </div>
+      <div class="col-sm-7">
+        <q-img src="tools/2.png" class="img-center q-mr-lg gt-xs" style="width: 90%;" />
       </div>
     </div>
 
