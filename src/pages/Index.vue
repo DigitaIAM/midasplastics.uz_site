8c0020275c4ccd3ff433d74c475691dd099fec94
Suxrob9966 <rabbimovich9966@gmail.com>
2022-12-24 00:27:14 -0600

    added some description text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center" style="background: #f5f7f5;">
     <!-- title -->
-    <div class="full-width header">
+    <div class="full-width header q-pb-md">
       <div class="title text-center text-h3 text-bold title-top text-red q-pa-xl">
         ПРАВИЛЬНАЯ УПАКОВКА
         <br />
@@ -12,8 +12,8 @@
           Мы производим пищевую упаковку для молочных предприятий.
         </span>
       </div>
-      <div class="title text-center text-h3 text-bold title-top text-white ">
-        <span class="bg-positive q-pa-md">НАШЕ ПРОИЗВОДСТВО</span>
+      <div class="title text-right text-h3 text-bold title-top text-white">
+        <span class="bg-positive q-pa-md" style="padding-right: 400px;">НАШЕ ПРОИЗВОДСТВО</span>
       </div>
     </div>
 
@@ -53,10 +53,9 @@
                 </svg> обеспечивает высокую производительность
               </q-card-section>
             </q-card>
-
           </div>
         </div>
-        <q-separator />
+        <q-separator style="width:35rem;" />
       </div>
       <div class="col-sm-7">
         <q-img src="tools/2.png" class="img-center q-mr-lg gt-xs" style="width: 90%;" />
@@ -104,7 +103,57 @@
         <q-separator />
       </div>
     </div>
+    <div class="full-width q-pt-lg">
+      <div class="title text-left text-h3 text-bold title-top text-white">
+        <span class="bg-positive q-pa-md" style="padding-left: 400px;">КАЧЕСТВО И БЕЗОПАСНОСТЬ</span>
+      </div>
+    </div>
 
+    <div class="row q-pt-xl">
+      <div class="col">
+        <div class="text-h5 text-justify" style="padding-left: 150px; padding-right: 150px;">
+          Все наше производство устроено так, как если бы мы производили детское питание - это позволяет нам
+          гарантировать чистоту и безопасность нашей продукции.
+        </div>
+      </div>
+    </div>
+
+    <div class="row q-pt-xl">
+      <div class="col">
+        <q-img src="product/a4.jpg" class="img-center q-ma-lg gt-xs" style="width: 900px; height: auto;" />
+      </div>
+    </div>
+
+    <div class="row q-pt-xl">
+      <div class="col">
+        <div class="text-h5 text-justify" style="padding-left: 150px; padding-right: 150px;">
+          Начиная от полимерной гранулы и заканчивая готовой упаковкой с декором - мы отслеживаем качество упаковки всех
+          стадиях производства. Это позволяет нам выпускать достойный продукт со стабильным качеством.
+        </div>
+      </div>
+    </div>
+
+    <div class="row q-pt-xl">
+      <div class="col">
+        <div class="text-h2 text-center text-indigo text-bold" style="padding-left: 300px; padding-right: 300px;">
+          ООО «MIDAS PLASTICS» - это производство полного цикла
+        </div>
+      </div>
+    </div>
+
+    <div class="row q-pt-xl">
+      <div class="col">
+        <div class="text-h5 text-justify" style="padding-left: 150px; padding-right: 150px;">
+          Весь путь от гранулы до готовой упаковки с печатью полностью проходит на одной территории, что позволяет нам
+          отслеживать весь процесс и производить качественный продукт.</div>
+      </div>
+    </div>
+
+    <div class="full-width q-pt-lg">
+      <div class="title text-right text-h3 text-bold title-top text-white">
+        <span class="bg-positive q-pa-md" style="padding-right: 600px;">ПРОДУКЦИЯ</span>
+      </div>
+    </div>
 
 
   </q-page>
