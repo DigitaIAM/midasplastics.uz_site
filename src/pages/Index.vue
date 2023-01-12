b0fead86077787aff8692a509abb033dbfc3f1c6
Suxrob9966 <rabbimovich9966@gmail.com>
2023-01-12 09:33:31 -0600

    designing for iphone 6/7/8 plus

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="flex flex-center" style="background: #f5f7f5;">
     <!-- title -->
-    <div class="full-width top-img header">
+    <div class="full-width top-img header gt-xs">
       <div class="title text-left text-h3 text-bold title-top q-pt-xl q-pl-xl">
-        <q-img src="/logo.svg" style="width: 15vw; height: auto" />
+        <q-img src="/logo.svg" style="width: 15vmax; height: auto" />
       </div>
       <div class="title text-center text-h4 text-bold title-bottom full-width q-pa-md">
         <span class="full-width">
@@ -12,8 +12,19 @@
       </div>
     </div>
 
+    <div class="full-width top-img header lt-sm">
+      <div class="title text-left text-h3 text-bold title-top q-pt-xl q-pl-xl">
+        <q-img src="/logo.svg" style="width: 15vmax; height: auto" />
+      </div>
+      <div class="title text-center text-h5 text-bold title-bottom full-width q-pa-md">
+        <span class="full-width">
+          Правильная упаковка для правильных продуктов
+        </span>
+      </div>
+    </div>
+
     <!-- products -->
-    <div class="row text-h6 text-center full-width q-ma-lg">
+    <div class="row text-h6 text-center full-width q-ma-lg gt-xs">
       <div class="fit row wrap justify-center content-center q-gutter-xl">
         <q-card class="my-card" v-for="group in groups" :key="group.img" @click="scrollToText(group.ref)">
           <q-img :src="group.img" :ratio="1" style="width:250px">
@@ -25,14 +36,26 @@
       </div>
     </div>
 
-    <div class="row full-width q-pt-lg">
+    <div class="row text-h6 text-center full-width q-my-lg lt-sm">
+      <div class="fit row wrap justify-center content-center q-gutter-y-lg">
+        <q-card class="my-card" v-for="group in groups" :key="group.img" @click="scrollToText(group.ref)">
+          <q-img :src="group.img" :ratio="1" style="width:250px">
+            <div class="absolute-bottom text-subtitle2 text-center">
+              {{ group.title }}
+            </div>
+          </q-img>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row full-width q-py-md">
       <div class="col-xs-12 col-sm-12 col-md-4"></div>
       <div class="col-xs-12 col-sm-12 col-md-8 bg-indigo-9 text-left text-h3 text-bold text-white q-pa-md headlines">
         &nbsp;Продукция
       </div>
     </div>
 
-    <div class="row q-py-lg">
+    <div class="row q-pt-md">
       <div class="col-xs-12">
         <div class="text-h5 product" ref="description" id="description" label="UF">
           <span class="text-bold">Стаканы c УФ печатью</span> является наиболее распространенным и экономичным
@@ -42,7 +65,7 @@
       </div>
     </div>
 
-    <div class="row q-pt-xl">
+    <div class="row q-pt-lg">
       <div class="col-xs-12">
         <div class="text-h5 product" ref="description1" id="description1">
           <span class="text-bold ">Стакан с картонной этикеткой – комбинация пластика и картона.</span> На наружную
@@ -52,7 +75,7 @@
       </div>
     </div>
 
-    <div class="row q-pt-xl">
+    <div class="row q-pt-lg">
       <div class="col-xs-12">
         <div class="text-h5 product" ref="description2" id="description2">
           <span class="text-bold ">Стакан с термоусадочной этикеткой.</span> На наружную поверхность полипропиленового
@@ -62,7 +85,7 @@
       </div>
     </div>
 
-    <div class="row q-py-xl">
+    <div class="row q-pt-lg">
       <div class="col-xs-12">
         <div class="text-h5 product" ref="description3" id="description3">
           <span class="text-bold ">Крышки на стаканы.</span> Мы производим полипропиленовые крышки «нахлобучки»
@@ -72,13 +95,13 @@
       </div>
     </div>
 
-    <q-btn icon="shopping_cart" no-caps color="deep-orange" glossy class="text-h4 q-mb-xl place-order"
+    <q-btn icon="shopping_cart" no-caps color="deep-orange" glossy class="text-h4 q-my-lg place-order"
       @click="scrollTo()">
       Оформить заказ
     </q-btn>
 
     <!-- quality & safety -->
-    <div class="row full-width q-pt-lg">
+    <div class="row full-width q-pt-md">
       <div class="col-xs-12 col-sm-12 col-md-8  bg-indigo-9 text-right text-h3 text-bold text-white q-pa-md headlines">
         Качество и безопасность
       </div>
@@ -97,16 +120,15 @@
       </div>
     </div>
 
-    <div class="row q-pt-xl">
+    <div class="row q-pt-lg">
       <div class="col">
-        <div class="text-h4 text-center text-indigo-9 text-bold slogan"
-          style="padding-left: 300px; padding-right: 300px;">
-          ООО «MIDAS PLASTICS» - это производство полного цикла
+        <div class="text-h5 text-center text-indigo-9 text-bold slogan">
+          ООО «MIDAS PLASTICS» — производство полного цикла
         </div>
       </div>
     </div>
 
-    <div class="row q-pt-xl">
+    <div class="row q-pt-lg">
       <div class="col">
         <div class="text-h5 text-justify safety">
           Весь путь от гранулы до готовой упаковки с печатью полностью проходит на одной территории, что позволяет нам
@@ -125,10 +147,10 @@
     <!-- description -->
     <!-- first card -->
     <div class="row full-width q-pt-lg flex-center">
-      <div class="col-sm-6">
-        <q-img src="tools/1.png" class="img-center q-ma-lg q-ml-xl" style="width: 80%;" />
+      <div class="col-xs-12 col-sm-6">
+        <q-img src="tools/1.png" class="list-photo q-ma-lg q-ml-xl" style="width: 80%;" />
       </div>
-      <div class="col-sm-6">
+      <div class="col-xs-12 col-sm-6">
         <div class="row q-px-lg q-pt-lg text-h5 text-left list-text">
           <div class="col-1 text-right">
             ✔&nbsp;
@@ -137,7 +159,7 @@
             оснащено лучшим в мире оборудованием европейского производства
           </div>
         </div>
-        <div class="row q-px-lg q-pt-lg text-h5 list-text">
+        <div class="row q-px-lg q-pt-lg text-h5 text-left list-text">
           <div class="col-1 text-right">
             ✔&nbsp;
           </div>
@@ -149,8 +171,8 @@
     </div>
 
     <!-- second card -->
-    <div class="row full-width q-pt-lg flex-center">
-      <div class="col-sm-6">
+    <div class="row full-width  flex-center">
+      <div class="col-xs-12 col-sm-6">
         <div class="row q-px-lg q-pt-lg text-h5 text-left list-text">
           <div class="col-1 text-right">
             ✔&nbsp;
@@ -162,46 +184,14 @@
         </div>
       </div>
 
-      <div class="col-sm-6 flex-center">
-        <q-img src="product/a1.png" class="img-center q-ma-lg gt-xs" style="width: 100%;" />
+      <div class="col-xs-12 col-sm-6 flex-center gt-xs">
+        <q-img src="product/a1.png" class="list-photo q-ma-lg" style="width: 80%;" />
+      </div>
+      <div class="col-xs-12 col-sm-6 flex-center lt-sm">
+        <q-img src="product/a1.png" class="list-photo q-ma-lg float-right" style="width: 80%;" />
       </div>
     </div>
 
-    <!-- price list -->
-    <div class="row full-width q-pt-lg">
-      <div class="col-xs-12 col-sm-12 col-md-8 bg-indigo-9 text-right text-h3 text-bold text-white q-pa-md headlines">
-        Прайс лист&nbsp;
-      </div>
-      <div class="col-xs-12 col-sm-12 col-md-4"></div>
-    </div>
-
-    <div>
-      <q-scroll-area style="height: 270px; width: 1100px;">
-        <div class="row q-pa-lg q-ml-lg">
-          <q-table title="Стакан с картонной этикеткой 230мл" title-class="text-bold" :rows="rows" :columns="columns"
-            row-key="name" hide-bottom class="my-table" />
-        </div>
-      </q-scroll-area>
-
-      <q-scroll-area style="height: 270px; max-width: 100%;">
-        <div class="row q-pa-lg q-ml-lg">
-          <q-table title="Стакан с картонной этикеткой 420мл" title-class="text-bold" :rows="rows_1"
-            :columns="columns_1" row-key="name" hide-bottom class="my-table" />
-        </div>
-      </q-scroll-area>
-
-      <q-scroll-area style="height: 270px; max-width: 100%;">
-        <div class="row q-pa-lg q-ml-lg">
-          <q-table title="Стакан с термоусадочной этикеткой 270мл" title-class="text-bold" :rows="rows_2"
-            :columns="columns_2" row-key="name" hide-bottom class="my-table" />
-        </div>
-      </q-scroll-area>
-
-      <div class="q-pa-lg q-ml-lg float-left">
-        <q-table title="Крышка полипропиленовая" title-class="text-bold" :rows="rows_3" :columns="columns_3"
-          row-key="name" hide-bottom class="my-table" />
-      </div>
-    </div>
 
     <!-- placing orders -->
     <div class="row full-width q-pt-lg">
@@ -225,7 +215,8 @@
           <div class="row">
             <div class="col-xs-1 col-sm-3 col-md-1 text-primary text-h6 icon"><span class="material-icons">phone&nbsp;
               </span></div>
-            <div class="col-xs-11 col-sm-9 col-md-11 text-h6 contacts"><a href="tel:+998933338575" style="color: darkorchid;">
+            <div class="col-xs-11 col-sm-9 col-md-11 text-h6 contacts"><a href="tel:+998933338575"
+                style="color: darkorchid;">
                 +998 93 333-85-75
               </a> <br />
               <a href="tel:+998937203338" style="color: darkorchid;">
@@ -246,7 +237,8 @@
           <div class="row">
             <div class="col-xs-1 col-sm-3 col-md-1 text-primary text-h6 icon"><span class="material-icons">mail&nbsp;
               </span></div>
-            <div class="col-xs-11 col-sm-9 col-md-11 text-h6 contacts"><a href="mailto:info@midasplastics.uz">info@midasplastics.uz</a>
+            <div class="col-xs-11 col-sm-9 col-md-11 text-h6 contacts"><a
+                href="mailto:info@midasplastics.uz">info@midasplastics.uz</a>
             </div>
           </div>
         </div>
@@ -304,282 +296,7 @@ const scrollToText = (data) => {
   setVerticalScrollPosition(target, offset, duration);
 }
 
-// price list
-// table 1
-const columns = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Статья затрат/Тираж',
-    align: 'left',
-    field: (row) => row.name,
-    style: 'font-size: 16px',
-    headerStyle: {
-      fontSize: '1.2em',
-      fontWeight: 'bold'
-    },
-  },
-  {
-    name: 'a', label: '50 000', field: 'a', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'b', label: '100 000', field: 'b', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'c', label: '150 000', field: 'c', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'd', label: '200 000', field: 'd', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'e', label: '250 000', field: 'e', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'f', label: '300 000', field: 'f', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'g', label: '350 000', field: 'g', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'h', label: '400 000', field: 'h', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-];
 
-const rows = [
-  {
-    name: 'Цена стакан + крышка, без НДС',
-    a: '858,72',
-    b: '735,36',
-    c: '685,17',
-    d: '665,29',
-    e: '653,33',
-    f: '647,80',
-    g: '643,09',
-    h: '638,66',
-  },
-  {
-    name: 'Цена стакан + крышка, с НДС',
-    a: '961,76',
-    b: '823,60',
-    c: '767,39',
-    d: '745,12',
-    e: '731,72',
-    f: '725,53',
-    g: '720,26',
-    h: '715,29',
-  },
-];
-
-// table 2
-const columns_1 = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Статья затрат/Тираж',
-    align: 'left',
-    field: (row) => row.name,
-    style: 'font-size: 16px',
-    headerStyle: {
-      fontSize: '1.2em',
-      fontWeight: 'bold'
-    }
-  },
-  {
-    name: 'a', label: '50 000', field: 'a', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'b', label: '100 000', field: 'b', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'c', label: '150 000', field: 'c', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'd', label: '200 000', field: 'd', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'e', label: '250 000', field: 'e', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'f', label: '300 000', field: 'f', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'g', label: '350 000', field: 'g', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'h', label: '400 000', field: 'h', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-];
-
-const rows_1 = [
-  {
-    name: 'Цена стакан + крышка, без НДС',
-    a: '1 078,31',
-    b: '947,89',
-    c: '892,85',
-    d: '858,94',
-    e: '843,54',
-    f: '838,44',
-    g: '834,17',
-    h: '830,75',
-  },
-  {
-    name: 'Цена стакан + крышка, с НДС',
-    a: '1 207,70',
-    b: '1 061,63',
-    c: '999,99',
-    d: '962,01',
-    e: '944,76',
-    f: '939,05',
-    g: '934,27',
-    h: '930,44',
-  },
-];
-
-// table 3
-const columns_2 = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Статья затрат/Тираж',
-    align: 'left',
-    field: (row) => row.name,
-    style: 'font-size: 16px',
-    headerStyle: {
-      fontSize: '1.2em',
-      fontWeight: 'bold'
-    }
-  },
-  {
-    name: 'a', label: '50 000', field: 'a', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'b', label: '100 000', field: 'b', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'c', label: '150 000', field: 'c', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'd', label: '200 000', field: 'd', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'e', label: '250 000', field: 'e', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'f', label: '300 000', field: 'f', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'g', label: '350 000', field: 'g', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-  {
-    name: 'h', label: '400 000', field: 'h', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-];
-
-const rows_2 = [
-  {
-    name: 'Цена стакан + крышка, без НДС',
-    a: '688,12',
-    b: '664,60',
-    c: '656,76',
-    d: '652,84',
-    e: '650,49',
-    f: '648,92',
-    g: '647,80',
-    h: '645,22',
-  },
-  {
-    name: 'Цена стакан + крышка, с НДС',
-    a: '770,69',
-    b: '744,35',
-    c: '735,57',
-    d: '731,18',
-    e: '728,54',
-    f: '726,79',
-    g: '725,53',
-    h: '722,64',
-  },
-];
-
-// table 4
-const columns_3 = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Статья затрат/Тираж',
-    align: 'left',
-    field: (row) => row.name,
-    style: 'font-size: 15px',
-    headerStyle: {
-      fontSize: '1.2em',
-      fontWeight: 'bold'
-    }
-  },
-  {
-    name: 'a', label: '50 000', field: 'a', style: 'font-size: 15px', headerStyle: {
-      fontSize: '1.2em'
-    }
-  },
-
-];
-
-const rows_3 = [
-  {
-    name: 'Итого, цена без НДС',
-    a: '130,64',
-  },
-  {
-    name: 'Итого, цена с НДС',
-    a: '146,31',
-  },
-];
 </script>
 
 <style lang="sass" scoped>
@@ -645,12 +362,10 @@ const rows_3 = [
 
   .slogan
       font-size: 26px
-
+      margin-left: 30px
+      margin-right: 30px
   .list-text
       font-size: 20px
-  .my-table
-      max-width: 340px
-      margin-left: 330px
 
   .contacts
       font-size: 20px
@@ -684,10 +399,6 @@ const rows_3 = [
   .list-text
       font-size: 20px
 
-  .my-table
-      max-width: 600px
-      margin-left: 220px
-
   .map
       width: 500px
       height: 400px
@@ -716,16 +427,11 @@ const rows_3 = [
 
   .list-text
       font-size: 20px
-  .my-table
-      max-width: 700px
-      margin-left: 180px
 
   .map
       width: 550px
       height: 500px
       margin-left: 125px
-
-  
   .icon
       text-align: right
 </style>
